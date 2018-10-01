bb6428c7190c58c30c8424e94b85921b27340aa3
Kyle Lim <kylelim8998@gmail.com>
2018-10-01 15:55:48 -0400

    search insert position golang solution
    Added a golang solution.

---
+++ Golang/search-insert-position.go
@@ -0,0 +1,40 @@
+
+// Time: O(log(n))
+// Space: O(1)
+func searchInsert(nums []int, target int) int {
+    hi := len(nums)-1
+    lo := 0
+    mid := hi/2
+    
+    if target > nums[hi] {
+        return hi+1
+    } 
+    
+    for lo <= hi {
+        if nums[mid]  ==  target{
+            return mid			
+        }else if nums[mid] < target{
+            lo = mid+1			
+        }else{
+            hi = mid-1		
+        }
+        mid = (hi-lo) + lo	
+    }
+    return lo
+}
+
+// O(n)
+// Space: O(1)
+func searchInsert(nums []int, target int) int {
+    // If target is greater than any value
+    if target > nums[len(nums)-1] {
+        return len(nums)
+    }
+    // Loop through array and compare values to target
+    for i := 0; i < len(nums); i++{
+        if nums[i] >= target {
+            return i
+        }
+    }
+    return 0
+}
